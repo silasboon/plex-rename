58d0da1a67295bb79d9a31dcec33771087236dde
silasboon <49536201+silasboon@users.noreply.github.com>
2022-10-19 23:29:47 -0700

    Create rename.py

---
+++ rename.py
@@ -0,0 +1,92 @@
+from distutils import extension
+import os
+import re
+import requests
+apiKey = 'api_key=08d85f47ee3b13f3aee2110785af86fa'
+apiUrl = 'https://api.themoviedb.org/3/'
+
+#response = requests.get(apiUrl, timeout=2)
+#print(response.json())
+#convert list to string
+def listToString(s):
+    string1 = '' 
+    return (string1.join(s))
+
+#clean up show name
+def cleanUpName(showName, showEpisdode):
+    #only capitalize first letter
+    showName = showName.title()
+    #concatinate show info and season info
+    fullName = showName + ' ' + showEpisdode
+    #replace '.' with a space 
+    fullName = fullName.replace('.', ' ')
+    #remove anything within parentheses
+    fullName = re.sub("[\(\[].*?[\)\]]", "", fullName)
+    #remove special characters
+    fullName = re.sub("-", '', fullName)
+    #remove extra spaces
+    fullName = " ".join(fullName.split())
+    return(fullName)
+
+#Get show season and episode and convert to upper case if not already 
+def showInfo(file):
+    showInfo = ''
+    #get SXXEXX information
+    upperCase = re.findall('S[0-9][0-9]E[0-9][0-9]', file)
+    lowerCase = re.findall('s[0-9][0-9]e[0-9][0-9]', file)
+    #convert to string
+    upperCase = listToString(upperCase)
+    lowerCase = listToString(lowerCase)
+    #if string is None then pass (uppercase)
+    if len(upperCase) == 0:
+        pass
+    #else return uppercase (no changes made)
+    else:
+        return(upperCase)
+    #if string is none then pass (lowercase)
+    if len(lowerCase) == 0:
+        pass
+    #else convert showInfo to string and then convert to uppercase and return showInfo 
+    else: 
+        showInfo = listToString(lowerCase)
+        showInfo = showInfo.upper()
+        return(showInfo)
+
+#Get show name information
+def getShowName(file):
+    #convert entire string to uppercase
+    tempShowName = file.upper()
+    #Split season info from the rest of the text
+    showName = re.split('S[0-9][0-9]E[0-9][0-9]', tempShowName)
+    #convert text to string
+    #showName = listToString(showName)
+    #Capitalize first letter of every word 
+
+    return showName
+
+def renameFile():
+    # Path to files
+    path = '/Users/silas/Torrents/Complete/'
+    files = os.listdir(path)
+    #loop through all files
+    for file in files:
+        #assign original name to variable 
+        original = file
+        #seperate extension from name
+        name, extension = os.path.splitext(file)
+        seasonEpisode = showInfo(file)
+        #get show name 
+        showName = getShowName(file)
+        #assign to new variables and remove extra words
+        SE = seasonEpisode
+        SN = listToString(showName[0])
+        #error check if SE does not exist (not a show file i.e a DS_Store file or script)
+        if SE == None:
+            pass 
+        else:  
+            #clean up and concat showName and seasonEpisode
+            finalName = cleanUpName(SN, SE)
+            #rename files 
+            os.rename(path + '/' + original, path + '/' + finalName + extension)
+
+renameFile()
